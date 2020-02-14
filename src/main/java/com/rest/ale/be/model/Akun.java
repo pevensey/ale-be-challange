0264a1b28470bf2d2032fff71590c9612e31120d
pevensey <yuliantopambudi45@gmail.com>
2020-02-14 17:11:20 +0700

    create class Akun

---
+++ src/main/java/com/rest/ale/be/model/Akun.java
@@ -0,0 +1,84 @@
+package com.rest.ale.be.model;
+
+import org.hibernate.annotations.GenericGenerator;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "akun")
+public class Akun {
+
+    @GeneratedValue(
+            strategy= GenerationType.AUTO,
+            generator="native"
+    )
+    @GenericGenerator(
+            name = "native",
+            strategy = "native"
+    )
+    private long id_akun;
+    @Column
+    private String username;
+    @Column
+    private String password;
+    @Column
+    private String nama;
+    @Column
+    private Enum role;
+    @Column(nullable = false, updatable = false)
+    @CreatedDate
+    private java.util.Date createdAt;
+    @Column(nullable = false)
+    @LastModifiedDate
+    private java.util.Date updatedAt;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public Enum getRole() {
+        return role;
+    }
+
+    public void setRole(Enum role) {
+        this.role = role;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
